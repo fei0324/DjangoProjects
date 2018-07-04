c8c01e5623f9c1ae06dcfee307ba54e7047c30df
fei0324 <fl7834a@student.american.edu>
2018-07-04 16:57:24 -0400

    first commit

--- TestMySQL/TestMySQLProject/TestMySQLProject/settings.py
+++ TestMySQL/TestMySQLProject/TestMySQLProject/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'TestMySQLDatabase',
         'USER': 'root',
-        'PASSWORD': 'he960324',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '3307',
     }
